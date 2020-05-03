345a5729df25c617ef63f718a6b17bd87c649699
hyeonju <guswn1281@naver.com>
2020-05-03 19:19:54 +0900

    day7

---
+++ day7/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('views', __dirname + '/view');
+app.set('view engine' , 'ejs');
+
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const musicRouter = require('./router/MusicRouter');
+app.use(musicRouter);
+
+module.exports = app;
---
+++ day7/model/MusicModel.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+class Music {
+    constructor() {
+        const data = fs.readFileSync('./model/data.json');
+        this.musics = JSON.parse(data)
+    }
+
+    // Promise 예제
+    getMusicList() {
+        if (this.musics) {
+            return this.musics;
+        }
+        else {
+            return [];
+        }
+    }
+
+    addMusic(title, singer, introduction) {
+        return new Promise((resolve, reject) => {
+            let last = this.musics[this.musics.length - 1];
+            let id = last.id + 1;
+
+            let newMusic = {id, title, singer, introduction};
+            this.musics.push(newMusic);
+
+            resolve(newMusic);
+        });
+    }
+
+    // Promise - Reject
+    getMusicDetail(musicId) {
+        return new Promise((resolve, reject) => {
+            for (var music of this.musics ) {
+                if ( music.id == musicId ) {
+                    resolve(music);
+                    return;
+                }
+            }
+            reject({msg:'Can not find music', code:404});
+        });
+    }
+}
+
+module.exports = new Music();
